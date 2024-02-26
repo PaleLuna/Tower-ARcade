f87fe14a112fb679a75f7e64aa5cd27a5c840825
PaleLuna <47419205+PaleLuna@users.noreply.github.com>
2024-02-26 17:09:26 +0300

    Using the EntryPoint pattern
    The template implementation is taken from my own architecture "MoonlightUnity-Architecture"

---
+++ Assets/_Scripts/GameScripts/GameAREntryPoint.cs
@@ -0,0 +1,15 @@
+using Cysharp.Threading.Tasks;
+using PaleLuna.Architecture.EntryPoint;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameAREntryPoint : EntryPoint
+{
+    protected override UniTask Setup()
+    {
+        return base.Setup();
+
+
+    }
+}
