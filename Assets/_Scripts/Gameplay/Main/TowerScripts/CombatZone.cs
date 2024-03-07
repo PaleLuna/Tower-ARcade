b787fb8278b1229950bb1b376b9316e26b4beb9e
PaleLuna <47419205+PaleLuna@users.noreply.github.com>
2024-03-07 14:51:04 +0300

    Initial implementation of CombatZone

--- Assets/_Scripts/Gameplay/Main/TowerScripts/CombatZone.cs
+++ Assets/_Scripts/Gameplay/Main/TowerScripts/CombatZone.cs
@@ -1,18 +1,63 @@
-using System.Collections;
-using System.Collections.Generic;
+using PaleLuna.DataHolder;
 using UnityEngine;
+using UnityEngine.Events;
 
+[RequireComponent(typeof(SphereCollider))]
 public class CombatZone : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Components")]
+    [SerializeField] private SphereCollider _sphereCollider;
+
+    private TowerConf _combatConf;
+
+    private readonly DataHolder<Enemy> _enemies = new();
+
+    #region [ Events ]
+    private readonly UnityEvent _firstEnemyEnter = new();
+    private readonly UnityEvent _lastEnemyExit = new();
+
+    #region [ Events Subscribe methods ]
+    public void SubscribeOnFirstEnter(UnityAction action) => _firstEnemyEnter.AddListener(action);
+    public void SubscribeOnLastExit(UnityAction action) => _lastEnemyExit.AddListener(action);
+
+    public void UnsubscribeOnFirstEnter(UnityAction action) => _firstEnemyEnter.RemoveListener(action);
+    public void UnsubscribeOnLastExit(UnityAction action) => _lastEnemyExit.RemoveListener(action);
+    #endregion
+    #endregion
+
+    public void Init(TowerConf combatConf)
     {
-        
+        _combatConf = combatConf;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnValidate()
     {
-        
+        _sphereCollider ??= GetComponent<SphereCollider>();
+    }
+
+    public Enemy GetEnemy() => _enemies.At(0);
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!TryGetEnemy(other, out Enemy enemy)) return;
+
+        _enemies.Registration(enemy);
+
+        if (_enemies.Count == 1) _firstEnemyEnter.Invoke();
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (!TryGetEnemy(other, out Enemy enemy)) return;
+
+        _enemies.Unregistration(enemy);
+
+        if (_enemies.Count == 0) _lastEnemyExit.Invoke();
+    }
+
+    private bool TryGetEnemy(Collider collider, out Enemy enemy)
+    {
+        enemy = collider.GetComponent<Enemy>();
+
+        return enemy != null;
     }
 }
