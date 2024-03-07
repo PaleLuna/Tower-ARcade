b02aad9a16a5190ba0156b14104ccaa967389b9c
PaleLuna <47419205+PaleLuna@users.noreply.github.com>
2024-03-07 19:01:51 +0300

    Preparation for the implementation of shooting

--- Assets/_Scripts/Gameplay/Main/TowerScripts/Tower.cs
+++ Assets/_Scripts/Gameplay/Main/TowerScripts/Tower.cs
@@ -2,17 +2,22 @@ using UnityEngine;
 
 public class Tower : MonoBehaviour
 {
+    [Header("Components")]
     [SerializeField] private TowerConf _towerConf;
     [SerializeField] private CombatZone _combatZone;
 
+    [Header("Objects")]
     [SerializeField] private Transform _towerHead;
+    [SerializeField] private Transform _Muzzel;
+
+    [Header("Bullet prefab")]
+    private Transform _bulletPrefab;
 
     private TowerStateHolder _stateHolder;
 
     public TowerConf towerConf => _towerConf;
     public CombatZone combatZone => _combatZone;
     public TowerStateHolder stateHolder => _stateHolder;
-
     public Transform towerHead => _towerHead;
 
     private void Start()
@@ -24,7 +29,7 @@ public class Tower : MonoBehaviour
 
     virtual public void Fire(Transform enemyTransform) //In the future it will be used for shooting
     {
-        Debug.Log($"Fire on {enemyTransform.gameObject.name}");
+        
     }
     virtual public void RotateHead(Transform target)
     {
