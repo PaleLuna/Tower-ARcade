98ddeb757c1bc1b8ab6e16fab9f35e1c3010a9f5
PaleLuna <47419205+PaleLuna@users.noreply.github.com>
2024-03-07 14:54:27 +0300

    Update Tower.cs

--- Assets/_Scripts/Gameplay/Main/TowerScripts/Tower.cs
+++ Assets/_Scripts/Gameplay/Main/TowerScripts/Tower.cs
@@ -3,6 +3,13 @@ using UnityEngine;
 public class Tower : MonoBehaviour
 {
     [SerializeField] private TowerConf _towerConf;
+    [SerializeField] private CombatZone _combatZone;
 
     public TowerConf towerConf => _towerConf;
+    public CombatZone combatZone => _combatZone;
+
+    virtual public void Fire(Transform enemyTransform) //In the future it will be used for shooting
+    {
+        Debug.Log($"Fire on {enemyTransform.gameObject.name}");
+    } 
 }
