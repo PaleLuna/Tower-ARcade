8531cf6b77225fb8dae2138d4159e50fb7484aa9
PaleLuna <47419205+PaleLuna@users.noreply.github.com>
2024-03-07 16:06:12 +0300

    Update TowerIdleState.cs

--- Assets/_Scripts/GameScripts/States/TowerStates/States/TowerIdleState.cs
+++ Assets/_Scripts/GameScripts/States/TowerStates/States/TowerIdleState.cs
@@ -3,4 +3,20 @@ using UnityEngine;
 public class TowerIdleState : TowerState
 {
     public TowerIdleState(Tower context) : base(context){}
+
+    public override void StateStart()
+    {
+        Debug.Log($"New State! {this}");
+        m_context.combatZone.SubscribeOnFirstEnter(OnEnemyEnter);
+    }
+
+    private void OnEnemyEnter()
+    {
+        m_context.stateHolder.ChangeState<TowerCombatState>();
+    }
+
+    public override void StateStop()
+    {
+        m_context.combatZone.UnsubscribeOnFirstEnter(OnEnemyEnter);
+    }
 }
