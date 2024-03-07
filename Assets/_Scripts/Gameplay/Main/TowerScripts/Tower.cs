2b74e14a5548c7c56a7faad2f41ca9a5d501ba8c
PaleLuna <47419205+PaleLuna@users.noreply.github.com>
2024-03-07 16:07:58 +0300

    Update Tower.cs

--- Assets/_Scripts/Gameplay/Main/TowerScripts/Tower.cs
+++ Assets/_Scripts/Gameplay/Main/TowerScripts/Tower.cs
@@ -5,11 +5,32 @@ public class Tower : MonoBehaviour
     [SerializeField] private TowerConf _towerConf;
     [SerializeField] private CombatZone _combatZone;
 
+    [SerializeField] private Transform _towerHead;
+
+    private TowerStateHolder _stateHolder;
+
     public TowerConf towerConf => _towerConf;
     public CombatZone combatZone => _combatZone;
+    public TowerStateHolder stateHolder => _stateHolder;
+
+    public Transform towerHead => _towerHead;
+
+    private void Start()
+    {
+        _stateHolder = new(this);
+
+        _combatZone.Init(_towerConf);
+    }
 
     virtual public void Fire(Transform enemyTransform) //In the future it will be used for shooting
     {
         Debug.Log($"Fire on {enemyTransform.gameObject.name}");
-    } 
+    }
+    virtual public void RotateHead(Transform target)
+    {
+        Vector3 direction = target.position - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(direction);
+
+        _towerHead.rotation = Quaternion.AngleAxis(rotation.eulerAngles.y, Vector3.up);
+    }
 }
