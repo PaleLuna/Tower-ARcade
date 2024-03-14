b3431088e55dc9f346582ce72187d0a646ef32a9
PaleLuna <47419205+PaleLuna@users.noreply.github.com>
2024-03-14 15:59:29 +0300

    Implementation of the enemy model

---
+++ Assets/_Scripts/ScriptableObjects/EnemyConf.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EnemyConf", menuName = "Configs/Enemy")]
+public class EnemyConf : ScriptableObject
+{
+    [Header("Mobility")]
+    private float _speed;
+
+    [Header("Health")]
+    [SerializeField, Min(1)]
+    private int _health = 1;
+
+    [Space]
+    [SerializeField, Min(1)]
+    private int _penaltyForPassing = 1;
+
+    public float speed => _speed;
+
+    public int health
+    {
+        get => _health;
+        set
+        {
+            _health = Mathf.Max(0, value);
+        }
+    }
+    public int penaltyForPassing => _penaltyForPassing;
+
+    public void Copy(EnemyConf other)
+    {
+        _speed = other._speed;
+        _health = other._health;
+        _penaltyForPassing = other._penaltyForPassing;
+    }
+}
