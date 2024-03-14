f2b1e69a549a592a53fbccc9ddf9a252ae2dd433
PaleLuna <47419205+PaleLuna@users.noreply.github.com>
2024-03-14 16:01:31 +0300

    Implementation of the Enemy class

--- Assets/_Scripts/Gameplay/Main/EnemyScripts/Enemy.cs
+++ Assets/_Scripts/Gameplay/Main/EnemyScripts/Enemy.cs
@@ -1,6 +1,36 @@
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+[RequireComponent (typeof(Rigidbody))]
+public class Enemy : MonoBehaviour, IDamagable
 {
-    
+    [Header("Config")]
+    [SerializeField]
+    private EnemyConf _baseEnemyConf;
+    private EnemyConf _currentEnemyConf;
+
+    [Header("Components")]
+    [SerializeField] Rigidbody _rigidbody;
+
+    private EnemyStateHolder _stateHolder;
+
+    #region [ MonoBehaviour methods ]
+    private void OnValidate()
+    {
+        _rigidbody ??= GetComponent<Rigidbody>();
+    }
+
+    private void Awake()
+    {
+        _currentEnemyConf = ScriptableObject.CreateInstance<EnemyConf>();
+        _currentEnemyConf.Copy(_baseEnemyConf);
+    }
+    #endregion
+
+    public void SetDamage(float damage)
+    {
+        _currentEnemyConf.health -= (int)damage;
+
+        if (_currentEnemyConf.health == 0)
+            _stateHolder.ChangeState<EnemyStateDeath>();
+    }
 }
