0620f70a57fc478b27cf1dac674255b5857fadb4
PaleLuna <47419205+PaleLuna@users.noreply.github.com>
2024-03-15 13:08:16 +0300

    Implementation EnemyStateWalk

--- Assets/_Scripts/GameScripts/States/EnemyStates/States/EnemyStateWalk.cs
+++ Assets/_Scripts/GameScripts/States/EnemyStates/States/EnemyStateWalk.cs
@@ -1,19 +1,51 @@
+using PaleLuna.Architecture.Controllers;
 using PaleLuna.Architecture.GameComponent;
+using Services;
+using UnityEngine;
 
 public class EnemyStateWalk : EnemyState, IFixedUpdatable
 {
-    public EnemyStateWalk(Enemy context)
-        : base(context) { }
+    private PathPoint _currentPathPoint;
+    private Vector3 _currentDirection;
+
+    public EnemyStateWalk(Enemy context, EnemyStateHolder hodler)
+        : base(context, hodler) { }
 
     public override void StateStart()
     {
+        ServiceManager
+            .Instance.GlobalServices.Get<GameController>()
+            .updatablesHolder.Registration(this);
+
+        m_context._pathPointReachedEvent.AddListener(ChangeDirection);
     }
+
     public void FixedFrameRun()
     {
-        throw new System.NotImplementedException();
+        m_context.rb.velocity = _currentDirection * m_context.enemyConf.speed;
+    }
+
+    private void ChangeDirection(PathPoint newPathPoint)
+    {
+        _currentPathPoint = newPathPoint.nextPoint;
+
+        if (_currentPathPoint == null)
+        {
+            m_hodler.ChangeState<EnemyStateIdle>();
+            return;
+        }
+
+        Vector3 targetPos = _currentPathPoint.transform.position;
+        targetPos.y = m_context.transform.position.y;
+
+        _currentDirection = (targetPos - m_context.transform.position).normalized;
     }
 
     public override void StateStop()
     {
+        ServiceManager
+            .Instance.GlobalServices.Get<GameController>()
+            .updatablesHolder.UnRegistration(this);
+        m_context._pathPointReachedEvent.RemoveListener(ChangeDirection);
     }
 }
