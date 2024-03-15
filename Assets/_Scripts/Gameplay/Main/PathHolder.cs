5705edad3bc292ac1ff8a28b1716f05c8440e496
PaleLuna <47419205+PaleLuna@users.noreply.github.com>
2024-03-15 13:09:44 +0300

    Update PathHolder.cs

--- Assets/_Scripts/Gameplay/Main/PathHolder.cs
+++ Assets/_Scripts/Gameplay/Main/PathHolder.cs
@@ -1,16 +1,28 @@
+using PaleLuna.Architecture.GameComponent;
 using PaleLuna.Architecture.Services;
+using Services;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PathHolder : MonoBehaviour, IService
+public class PathHolder : MonoBehaviour, IService, IStartable
 {
     [SerializeField]
     private List<Path> _paths = new();
+
+    private bool _isStart = false;
+    public bool IsStarted => _isStart;
 
     private void OnValidate()
     {
         foreach (Path item in _paths)
             item.Refresh();
+    }
+
+    public void OnStart()
+    {
+        ServiceManager.Instance.SceneLocator.Registarion(this);
+
+        _isStart = true;
     }
 
     public Path GetPath(int pathIndex)
@@ -19,4 +31,6 @@ public class PathHolder : MonoBehaviour, IService
             return _paths[pathIndex];
         return null;
     }
+
+    
 }
