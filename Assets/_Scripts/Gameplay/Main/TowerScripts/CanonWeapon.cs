8dfaed8e42df032ed8ac6c52ec10d929bb9e1554
PaleLuna <47419205+PaleLuna@users.noreply.github.com>
2024-03-15 16:09:45 +0300

    Allocation of a separate class responsible for shooting

---
+++ Assets/_Scripts/Gameplay/Main/TowerScripts/CanonWeapon.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class CanonWeapon : TowerWeapon
+{
+    public CanonWeapon(AmmunitionHolder context, TowerConf towerConf)
+        : base(context, towerConf) { }
+
+    public override void Fire(Enemy target, Vector3 muzzelPos) 
+    {
+        if (!m_context || !m_readyToFire) return;
+
+        Shell shell = m_context.GetShell();
+
+        Vector3 direction = PredictTheTargetPosition(target, muzzelPos);
+        shell.transform.position = muzzelPos;
+
+        shell.ThrowThis(direction, m_towerConf.initialSpeed, m_towerConf.damageByHit);
+
+        m_context.StartCoroutine(CoolDown());
+    }
+}
