aeda192c9e1281369129231e542bdd15e0284d35
PaleLuna <47419205+PaleLuna@users.noreply.github.com>
2024-03-15 13:10:15 +0300

    Implementation EnemyHolder

---
+++ Assets/_Scripts/Gameplay/Main/EnemyScripts/EnemyHolder.cs
@@ -0,0 +1,81 @@
+using PaleLuna.Architecture.GameComponent;
+using PaleLuna.Architecture.Services;
+using PaleLuna.DataHolder;
+using Services;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHolder : MonoBehaviour, IStartable, IService
+{
+    [SerializeField]
+    private Enemy _enemyPrefab;
+
+    [SerializeField]
+    private int _maxEnemiesOnLevel = 10;
+
+    [SerializeField, Tooltip("The interval between spawns in seconds")]
+    private int _intervalBetweenSpawns = 5;
+
+    private Queue<Enemy> _enemiesToRespawn;
+
+    private Transform _startPointTransfrom;
+
+    private Coroutine _respawnEnemies;
+
+    private bool _isStart = false;
+    public bool IsStarted => _isStart;
+
+    public void OnStart()
+    {
+        if (_isStart || !enabled) return;
+
+        ServiceLocator sceneLocator = ServiceManager.Instance.SceneLocator;
+
+        sceneLocator.Registarion(this);
+
+        _startPointTransfrom = sceneLocator.Get<PathHolder>().GetPath(0).GetFirst().transform;
+
+        _enemiesToRespawn = new(_maxEnemiesOnLevel);
+
+        StartCoroutine(SpawnEnemies());
+
+        GameEvents.enemyDeathEvent.AddListener(OnEnemyDeath);
+
+        _isStart = true;
+    }
+
+    private void OnEnemyDeath(Enemy enemy)
+    {
+        _enemiesToRespawn.Enqueue(enemy);
+
+        if (_respawnEnemies != null) return;
+
+        _respawnEnemies = StartCoroutine(RespawnEnemies());
+    }
+
+    private IEnumerator SpawnEnemies()
+    {
+        for(int i = 0; i < _maxEnemiesOnLevel; i++)
+        {
+            Enemy enemy = Instantiate(_enemyPrefab, transform);
+            enemy.gameObject.name = $"enemy {i}";
+            enemy.Respawn(_startPointTransfrom.position);
+
+            yield return new WaitForSeconds(_intervalBetweenSpawns);
+        }
+    }
+
+    private IEnumerator RespawnEnemies()
+    {
+        while(_enemiesToRespawn.Count > 0)
+        {
+            yield return new WaitForSeconds(_intervalBetweenSpawns);
+
+            Enemy enemy = _enemiesToRespawn.Dequeue();
+            enemy.Respawn(_startPointTransfrom.position);
+        }
+
+        _respawnEnemies = null;
+    }
+}
