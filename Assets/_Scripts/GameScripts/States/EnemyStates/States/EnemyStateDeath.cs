8111322d800d8b0bd65e3f8083b2ddbdae0f7695
PaleLuna <47419205+PaleLuna@users.noreply.github.com>
2024-03-15 13:10:01 +0300

    Update EnemyStateDeath.cs

--- Assets/_Scripts/GameScripts/States/EnemyStates/States/EnemyStateDeath.cs
+++ Assets/_Scripts/GameScripts/States/EnemyStates/States/EnemyStateDeath.cs
@@ -1,10 +1,18 @@
+using UnityEngine;
+
 public class EnemyStateDeath : EnemyState
 {
-    public EnemyStateDeath(Enemy context)
-        : base(context) { }
+    public EnemyStateDeath(Enemy context, EnemyStateHolder holder)
+        : base(context, holder) { }
 
     public override void StateStart()
     {
+        Dead();
+    }
+
+    private void Dead()
+    {
+        GameEvents.enemyDeathEvent.Invoke(m_context);
         m_context.gameObject.SetActive(false);
     }
 }
