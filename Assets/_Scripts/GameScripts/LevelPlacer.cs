08acc4dc0b671e5efbba16fa4aa7398be8198900
PaleLuna <47419205+PaleLuna@users.noreply.github.com>
2024-03-17 12:36:10 +0300

    Refactoring

--- Assets/_Scripts/GameScripts/LevelPlacer.cs
+++ Assets/_Scripts/GameScripts/LevelPlacer.cs
@@ -3,26 +3,33 @@ using UnityEngine;
 
 public class LevelPlacer : MonoBehaviour, IStartable
 {
-    public bool IsStarted => throw new System.NotImplementedException();
+    
 
+    [SerializeField]
     private GameObject _levelPrefab;
     private GameObject _levelObj;
     
     private bool _isStart = false;
+
+    public bool IsStarted => _isStart;
+
     public void OnStart()
     {
-        
+        if(_isStart) return;
+        _isStart = true;
     }
     
-    public void PlaceLevelIfNot(Vector3 point)
+    public void PlaceLevel(Vector3 point)
     {
-        print("Tap");
-        if (_levelObj != null)
-            return;
-
-        _levelObj = Instantiate(_levelPrefab);
-        _levelPrefab = null;
+        if (_levelObj == null)
+            CreateLevel(point);
 
         _levelObj.transform.position = point;
+    }
+
+    private void CreateLevel(Vector3 spawnPoint)
+    {
+        _levelObj = Instantiate(_levelPrefab);
+        _levelPrefab = null;
     }
 }
