843159e10984d63388a42e0f39ddff984c74af18
PaleLuna <47419205+PaleLuna@users.noreply.github.com>
2024-03-17 12:36:20 +0300

    Update InputHandler.cs

--- Assets/_Scripts/GameScripts/InputHandler.cs
+++ Assets/_Scripts/GameScripts/InputHandler.cs
@@ -4,30 +4,45 @@ using UnityEngine;
 public class InputHandler : MonoBehaviour, IStartable
 {
     [SerializeField]
-    private GameObject _levelPrefab;
-
-    [SerializeField]
     private RayScanner _screenTouchDetector;
 
     [SerializeField]
     private LevelPlacer _levelPlacer;
-
-    private GameObject _levelObj;
 
     private bool _isStart = false;
     public bool IsStarted => _isStart;
 
     public void OnStart()
     {
-        _screenTouchDetector.SubscribeOnPlaneDetect(_levelPlacer.PlaceLevelIfNot);
-        _screenTouchDetector.SubscribeOnGODetect(OnTapReaction);
+        if(_isStart) return;
+
+        OnLevelRestart();
+
+        GameEvents.gameRestart.AddListener(OnLevelRestart);
+        GameEvents.gameDefeatEvent.AddListener(OnGameDefeat);
+        GameEvents.levelConfirmEvent.AddListener(OnLevelPlaceConfirm);
+
+        _isStart = true;
     }
 
 
+    #region [ Event reactions ]
     private void OnTapReaction(GameObject gObj)
     {
         IInteractable interactable = gObj.GetComponent<IInteractable>();
         print($"ObjTap {interactable}");
         interactable?.Interact();
     }
+    private void OnLevelPlaceConfirm(){
+        _screenTouchDetector.UnsubscribeOnPlaneDetect(_levelPlacer.PlaceLevel);
+    }
+
+    private void OnLevelRestart(){
+        _screenTouchDetector.SubscribeOnPlaneDetect(_levelPlacer.PlaceLevel);
+        _screenTouchDetector.SubscribeOnGODetect(OnTapReaction);
+    }
+    private void OnGameDefeat(){
+        _screenTouchDetector.UnsubscribeOnGODetect(OnTapReaction);
+    }
+    #endregion
 }
