9f2d053d5cc02916cd2cc52af623b1b0a11570e4
PaleLuna <47419205+PaleLuna@users.noreply.github.com>
2024-03-19 19:21:29 +0300

    Level characteristics are now stored in the Game class

--- Assets/_Scripts/GameScripts/Game.cs
+++ Assets/_Scripts/GameScripts/Game.cs
@@ -5,8 +5,15 @@ using UnityEngine;
 
 public class Game : MonoBehaviour, IService, IStartable
 {
+    [SerializeField]
+    private LevelConfig _levelConf;
+
     private bool _isStart = false;
+
     public bool IsStarted => _isStart;
+
+    public LevelConfig levelConfig => _levelConf;
+
 
     public void OnStart()
     {
@@ -15,6 +22,8 @@ public class Game : MonoBehaviour, IService, IStartable
         ServiceManager.Instance.SceneLocator.Registarion(this);
 
         GameEvents.gameDefeatEvent.AddListener(OnGameDefeat);
+
+        _levelConf.levelPrefab.SetMaxHP(_levelConf.maxBaseHealthPoint);
 
         _isStart = true;
     }
