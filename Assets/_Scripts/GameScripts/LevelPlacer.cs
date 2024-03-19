9a6c8fbe1b88a7fb8ddabed1df410b150cd2051c
PaleLuna <47419205+PaleLuna@users.noreply.github.com>
2024-03-19 10:18:58 +0300

    added RotateToCam method
    added a method that turns the level towards the camera

--- Assets/_Scripts/GameScripts/LevelPlacer.cs
+++ Assets/_Scripts/GameScripts/LevelPlacer.cs
@@ -1,4 +1,5 @@
 using PaleLuna.Architecture.GameComponent;
+using Services;
 using UnityEditor;
 using UnityEngine;
 
@@ -9,6 +10,8 @@ public class LevelPlacer : MonoBehaviour, IStartable
     [SerializeField]
     private GameObject _levelPrefab;
     private GameObject _levelObj;
+
+    private CameraScript _camera;
     
     private bool _isStart = false;
 
@@ -17,6 +20,9 @@ public class LevelPlacer : MonoBehaviour, IStartable
     public void OnStart()
     {
         if(_isStart) return;
+
+        _camera = ServiceManager.Instance.SceneLocator.Get<CameraScript>();
+
         _isStart = true;
     }
     
@@ -26,14 +32,23 @@ public class LevelPlacer : MonoBehaviour, IStartable
             CreateLevel(point);
 
         _levelObj.transform.position = point;
-    }
+        RotateToCam();
 
+    }
     private void CreateLevel(Vector3 spawnPoint)
     {
         _levelObj = Instantiate(_levelPrefab);
         _levelPrefab = null;
 
         GameEvents.levelPlaceFirstly.Invoke();
+    }
+
+    private void RotateToCam()
+    {
+        Vector3 direction = (_camera.transform.position - _levelObj.transform.position).normalized;
+        Quaternion rotation = Quaternion.LookRotation(direction*-1);
+
+        _levelObj.transform.rotation = Quaternion.AngleAxis(rotation.eulerAngles.y, Vector3.up);
     }
 
     [ContextMenu("Test")]
