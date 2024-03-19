b291f38296d5432c7b7be1b2222250a901b55f63
PaleLuna <47419205+PaleLuna@users.noreply.github.com>
2024-03-19 17:44:50 +0300

    Refactoring + added new fields
    Preparing for the introduction of economics

--- Assets/_Scripts/ScriptableObjects/EnemyConf.cs
+++ Assets/_Scripts/ScriptableObjects/EnemyConf.cs
@@ -1,36 +1,57 @@
+using System;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "EnemyConf", menuName = "Configs/Enemy")]
 public class EnemyConf : ScriptableObject
 {
-    [Header("Mobility")]
+    [Header("Main characteristics")]
     [SerializeField]
-    private float _speed;
+    private EnemyMainCharacterisctis _mainCharacteristics;
 
-    [Header("Health")]
-    [SerializeField, Min(1)]
-    private int _health = 1;
-
-    [Space]
+    [Header("Penalties and awards")]
     [SerializeField, Min(1)]
     private int _penaltyForPassing = 1;
+    [SerializeField, Min(1)]
+    private int _awardForKill;
 
-    public float speed => _speed;
 
-    public int health
-    {
-        get => _health;
-        set
-        {
-            _health = Mathf.Max(0, value);
-        }
-    }
-    public int penaltyForPassing => _penaltyForPassing;
+    #region [ Properties ]
+        #region [ Main characteristics properties ]
+            public float speed => _mainCharacteristics.speed;
+            public int health
+            {
+                get => _mainCharacteristics.health;
+                set
+                {
+                    _mainCharacteristics.health = Mathf.Max(0, value);
+                }
+            }
+        #endregion
+
+        public int penaltyForPassing => _penaltyForPassing;
+        public int awardForKill => _awardForKill;
+    #endregion
 
     public void Copy(EnemyConf other)
     {
-        _speed = other._speed;
-        _health = other._health;
+        _mainCharacteristics.Copy(other._mainCharacteristics);
         _penaltyForPassing = other._penaltyForPassing;
+    }
+}
+
+[Serializable]
+public struct EnemyMainCharacterisctis
+{
+    [Header("Mobility")]
+    public float speed;
+
+    [Header("Health")]
+    [Min(1)]
+    public int health;
+
+    public void Copy(EnemyMainCharacterisctis other)
+    {
+        speed = other.speed;
+        health = other.health;
     }
 }
--- Assets/_Scripts/ScriptableObjects/LevelConfig.cs
+++ Assets/_Scripts/ScriptableObjects/LevelConfig.cs
@@ -1,14 +1,37 @@
+using System;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "LevelConf", menuName = "Configs/Level")]
 public class LevelConfig : ScriptableObject
 {
+    [Header("Level start characteristics")]
     [SerializeField]
-    private int _maxBaseHealthPoint;
+    LevelStartCharacteristics _levelStartChar;
 
-    public int maxBaseHealthPoint => _maxBaseHealthPoint;
+    #region Properties
+        #region [ Level start properties  ]
+            public int maxBaseHealthPoint => _levelStartChar.maxBaseHealthPoint;
+            public int startBalance => _levelStartChar.startBalance;
+        #endregion
+    #endregion
 
     public void Copy(LevelConfig other){
-        _maxBaseHealthPoint = other._maxBaseHealthPoint;
+        _levelStartChar.Copy(other._levelStartChar);
     }
+}
+
+[Serializable]
+public struct LevelStartCharacteristics 
+{
+    [Header("Base properties"), Min(1)]
+    public int maxBaseHealthPoint;
+    [Header("Wallet properties")]
+    public int startBalance;
+
+    public void Copy(LevelStartCharacteristics other)
+    {
+        maxBaseHealthPoint = other.maxBaseHealthPoint;
+        startBalance = other.startBalance;
+    }
+
 }
