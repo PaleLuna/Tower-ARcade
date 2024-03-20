d9e1c6a0ef25f87af1b0daba9c95b157f9fcf24b
PaleLuna <47419205+PaleLuna@users.noreply.github.com>
2024-03-20 21:52:35 +0300

    Added reaction to pause in game

--- Assets/_Scripts/GameScripts/States/TowerStates/States/TowerCombatState.cs
+++ Assets/_Scripts/GameScripts/States/TowerStates/States/TowerCombatState.cs
@@ -2,7 +2,7 @@ using PaleLuna.Architecture.Controllers;
 using PaleLuna.Architecture.GameComponent;
 using Services;
 
-public class TowerCombatState : TowerState, IUpdatable
+public class TowerCombatState : TowerState, IUpdatable, IPausable
 {
     private GameController _gameController;
 
@@ -14,7 +14,8 @@ public class TowerCombatState : TowerState, IUpdatable
 
     public override void StateStart()
     {
-        _gameController.updatablesHolder.Registration(this);
+        UpdateRegistration();
+        PausableRegistration();
         m_context.combatZone.SubscribeOnLastExit(OnNoEnemy);
     }
 
@@ -33,7 +34,29 @@ public class TowerCombatState : TowerState, IUpdatable
 
     public override void StateStop()
     {
-        _gameController.updatablesHolder.UnRegistration(this);
+        UpdateUnregistation();
+        PausableUnregistration();
         m_context.combatZone.UnsubscribeOnLastExit(OnNoEnemy);
     }
+
+    public void OnPause()
+    {
+        UpdateUnregistation();
+    }
+
+    public void OnResume()
+    {
+        UpdateRegistration();
+    }
+
+
+    private void PausableRegistration() =>
+        _gameController.pausablesHolder.Registration(this);
+    private void PausableUnregistration() =>
+        _gameController.pausablesHolder.Unregistration(this);
+
+    private void UpdateRegistration() =>
+        _gameController.updatablesHolder.Registration(this);
+    private void UpdateUnregistation() =>
+        _gameController.updatablesHolder.UnRegistration(this);
 }
